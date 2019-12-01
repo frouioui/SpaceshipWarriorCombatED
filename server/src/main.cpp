0099c0c5f0001d3afb6cefb5bc0cae84e24d054d
Florent Poinsard <florent.poinsard@outlook.fr>
2019-12-01 21:40:45 +0100

    [ADD] - Prog argument

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -4,16 +4,21 @@
 
 int main(int ac, char **argv)
 {
-    (void)ac;
-    (void)argv;
+    if (ac != 2) {
+        std::cout << "usage: ./r-type_server 2000" << std::endl;
+        return 84;
+    }
+
+    std::string port(argv[1]);
     std::srand(std::time(nullptr)); 
     try {
         boost::asio::io_context io_context;
-        UDPInfo info = {io_context, 2000};
+        UDPInfo info = {io_context, static_cast<short>(std::stoi(port))};
         Server s(info);
         s.run();
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
+        std::cerr << "SERVER - ERROR" << std::endl;
     }
     return (0);
 }
